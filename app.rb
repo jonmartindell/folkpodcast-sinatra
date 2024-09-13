021573c6eebe5ec1e7c29a608ebaa8768d98fc82
Jonathan Martindell <jonathan>
2024-09-12 20:27:14 -0400

    add ability to update rss xml

--- app.rb
+++ app.rb
@@ -7,7 +7,23 @@ get "/" do
 end
 
 get "/podcast.xml" do
-  send_file "data/podcast.xml"
+  rss_generator = RssGenerator.new
+  if rss_generator.days_since_update > 1
+    # try to update the list of songs
+    existing_files = rss_generator.items.map(&:title)
+    gdrive = GDrive.new
+    gdrive.list_files.each do |show_filename|
+      title = show_filename.sub(".mp3", "")
+      if !existing_files.include?(title)
+        file = gdrive.read_file(show_filename)
+        date = Date.parse(title).to_time
+        show_url = "https://folkpodcast.up.railway.app/show/#{show_filename}"
+        rss_generator.add_show(title: title, url: show_url, date: date, length_bytes: file.size)
+      end
+    end
+    rss_generator.save!
+  end
+  send_file RssGenerator::DEFAULT_FILENAME
 end
 
 get "/show/*.*" do |path, ext|
--- lib/rss_generator.rb
+++ lib/rss_generator.rb
@@ -1,13 +1,19 @@
 require "rss"
+require "date"
 require "forwardable"
 
 class RssGenerator
   extend Forwardable
-  DEFAULT_FILENAME = "recordings/rss.xml"
+  DEFAULT_FILENAME = "data/podcast.xml"
   def_delegators :@rss, :channel, :items
 
   def initialize(file: DEFAULT_FILENAME)
     @rss = RSS::Parser.parse(File.read(file))
+    @mtime = File.new(file).mtime
+  end
+
+  def days_since_update
+    (Date.parse(Time.now.to_s) - Date.parse(@mtime.to_s)).to_i
   end
 
   def add_show(title:, url:, date:, length_bytes:)
@@ -15,7 +21,6 @@ class RssGenerator
     new_item.title = title
     new_item.description = title
     new_item.pubDate = date.utc
-    new_item.guid.content = SecureRandom.uuid
     new_item.enclosure.url = url
     new_item.enclosure.length = length_bytes
     items << new_item
